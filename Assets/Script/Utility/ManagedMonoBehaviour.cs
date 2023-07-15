d5e03ccccdbe70ef5ebeafb233ebedfcbc0a3b82
高橋和仁 <122860830+KazuhitoTa@users.noreply.github.com>
2023-07-15 18:14:41 +0900

    gomi

--- Assets/Script/Utility/ManagedMonoBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ManagedMonoBehaviour : MonoBehaviour
-{
-    public virtual void Initialize() {}
-    public virtual void ManagedUpdate() {} 
-}
