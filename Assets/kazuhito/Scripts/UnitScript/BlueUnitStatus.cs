619d443304c6e26897f2b90006247287e4ba2a35
高橋和仁 <122860830+KazuhitoTa@users.noreply.github.com>
2023-07-15 18:40:28 +0900

    Revert "gomi"
    This reverts commit 1a4cf1494159d79eb8771adc89d5ee9f419658ab, reversing
    changes made to c6577f81b95120e7e76b7f5d2ef0bcd1077cd152.

--- Assets/kazuhito/Scripts/UnitScript/BlueUnitStatus.cs
+++ Assets/kazuhito/Scripts/UnitScript/BlueUnitStatus.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/kazuhito/Scripts/UnitScript/BlueUnitStatus.cs
 public class BlueUnitStatus : MonoBehaviour
-========
-public class UIManager : MonoBehaviour
->>>>>>>> main:Assets/Script/Scene/Menu/UIManager.cs
 {
     
 }
